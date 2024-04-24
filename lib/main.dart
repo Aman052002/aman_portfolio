e2bb2defc9c5bc7a778b6826f7a8ed903c695aae
amand <aman.dollop@gmail.com>
2024-04-24 14:44:05 +0530

    Port Folio

---
+++ lib/main.dart
@@ -0,0 +1,171 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'theme/responsive_screen_provider.dart';
+import 'theme/app_theme.dart';
+import 'widgets/nav_bar.dart';
+import 'desktop/ds_1_header.dart';
+import 'desktop/ds_2_about_me.dart';
+import 'desktop/ds_3_education.dart';
+import 'desktop/ds_4_experience.dart';
+import 'desktop/ds_7_contact.dart';
+import 'mobile/ms_1_header.dart';
+import 'mobile/ms_2_about_me.dart';
+import 'mobile/ms_3_education.dart';
+import 'mobile/ms_4_experience.dart';
+import 'mobile/ms_7_contact.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aman Rathore',
+      debugShowCheckedModeBanner: false,
+      theme: AppThemeData.darkTheme,
+      home: const Splash(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pop(context);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/splash.png',height: double.infinity),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showBackToTopButton = false;
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(
+        () {
+          setState(
+            () {
+              if (_scrollController.offset >= 300) {
+                _showBackToTopButton = true;
+              } else {
+                _showBackToTopButton = false;
+              }
+            },
+          );
+        },
+      );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(0,
+        duration: const Duration(milliseconds: 1000), curve: Curves.easeInOut);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget desktopUI() {
+      return ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: const [
+          DS1Header(),
+          DS2AboutMe(),
+          DS3Education(),
+          DS4Experience(),
+          DS7Contact(),
+        ],
+      );
+    }
+
+    Widget mobileUI() {
+      return ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: const [
+          MS1Header(),
+          MS2AboutMe(),
+          MS3Education(),
+          MS4Experience(),
+          MS7Contact(),
+        ],
+      );
+    }
+
+    return Scaffold(
+      appBar: ResponsiveScreenProvider.isDesktopScreen(context)
+          ? null
+          : AppBar(elevation: 0.0),
+      drawer: ResponsiveScreenProvider.isDesktopScreen(context)
+          ? null
+          : NavBar().mobileNavBar(),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          color: AppThemeData.backgroundGrey,
+          child: ResponsiveScreenProvider.isDesktopScreen(context)
+              ? desktopUI()
+              : mobileUI(),
+        ),
+      ),
+      floatingActionButton: _showBackToTopButton == false
+          ? null
+          : FloatingActionButton(
+              onPressed: _scrollToTop,
+              tooltip: 'Go to top',
+              backgroundColor: AppThemeData.buttonPrimary,
+              foregroundColor: AppThemeData.iconSecondary,
+              child: const Icon(
+                Icons.arrow_upward_rounded,
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/statics/data_values.dart
@@ -0,0 +1,121 @@
+class DataValues {
+  // About App
+  static const String appName = 'Aman Rathore';
+  static const String appNameLong = 'aman.dev';
+  static const String appDescription = 'Hello there, I\'m Aman Rathore. This is my personal website, which contains public information that anyone can find on the internet.';
+  static const String appDeveloper = 'Aman Rathore';
+  static const String appPackage = 'dev.aman.aman_dev';
+  static const String appVersion = '1.1.2';
+  static const String appBuildNumber = '1';
+
+  // External links
+  static Uri githubURL = Uri.parse('https://github.com/');
+  static Uri linkedinURL = Uri.parse('https://www.linkedin.com/in/aman-rathore-51b981287');
+  static Uri twitterURL = Uri.parse('https://twitter.com/');
+  static Uri telegramURL = Uri.parse('https://t.me/');
+  static Uri facebookURL = Uri.parse('https://facebook.com/');
+  static Uri instagramURL = Uri.parse('https://instagram.com/');
+  static Uri repoURL = Uri.parse('https://github.com/');
+
+  // Header
+  static const String headerGreetings = 'Hello, I\'m';
+  static const String headerName = 'Aman Rathore';
+  static const String headerTitle = 'Developer';
+
+  // Navigation Bar
+  static const String navBarAboutMe = 'About Me';
+  static const String navBarEducation = 'Education';
+  static const String navBarExperience = 'Experience';
+  static const String navBarContactMe = 'Contact Me';
+
+  // About me
+  static const String aboutMeTitle = 'About Me';
+  static const String aboutMeDescription = 'This website includes my public details, which anyone can find on the internet. So if you are looking for my personal information, such as my bio, education, experience, and other things that may make you wonder, feel free to scroll up and down to find out about me.';
+  static const String aboutMeBiographyTitle = 'Biography';
+  static const String aboutMeBiographyDescription =
+      'Hi there! I\'m a computing student, solution developer, and community volunteer. I believe that personal growth and learning are essential for success, and I have dedicated my life to achieving both. '
+      'Because of that, I\'m always looking to learn new things and apply them to whatever I\'m doing. '
+      'Technology is always igniting my passion for the positive impact it can have, and I\'m excited to continue growing my skills and knowledge, so I started my career as a techie. '
+      'In addition to my studies, I\'m also passionate about giving back to various communities and helping others, which is why I volunteer my time to help out whenever I can, and I believe that this work is vital in helping to create a better future for everyone.';
+  static const String aboutMeFullNameTitle = 'Full Name';
+  static const String aboutMeFullNameDescription = 'Aman Rathore';
+  static const String aboutMeNwITitle = 'Name with Initials';
+  static const String aboutMeNwIDescription = 'Aman Rathore';
+  static const String aboutMeFnLTitle = 'First and Last Name';
+  static const String aboutMeFnLDescription = 'Aman Rathore';
+  static const String aboutMeDobTitle = 'Birthday';
+  static const String aboutMeDobDescription = 'May 03, 2003';
+  static const String aboutMeLanguageTitle = 'Languages';
+  static const String aboutMeLanguageDescription = 'Hindi\n'
+      'English';
+  static const String aboutMeNationalityTitle = 'Nationality';
+  static const String aboutMeNationalityDescription = 'Indian';
+  static const String aboutMeGenderTitle = 'Gender';
+  static const String aboutMeGenderDescription = 'Male (He/Him/His)';
+  static const String aboutMeLocationTitle = 'Current Locations';
+  static const String aboutMeLocationDescription = 'Indore, Madhya Pradesh';
+  static const String aboutMeWorkDomainTitle = 'Working Domain';
+  static const String aboutMeWorkDomainDescription = 'aman.dev\n'
+      '08:00 - 18:00 (GMT+5:30)';
+  static const String aboutMeStudentTitle = 'Computing Student';
+  static const String aboutMeStudentDescription = 'I study computing technologies as a student by focusing on learning how computer systems work, from the physical (hardware) to the apps and programs (software) that users interact with, and I learn all about critical thinking, problem-solving, and analytical research, regardless of whether I apply all the computing principles to solve problems in scenarios.';
+  static const String aboutMeDeveloperTitle = 'Solution Developer';
+  static const String aboutMeDeveloperDescription = 'I use various tools for creating and implementing innovative solutions to problems in order to accomplish particular goals. I employ a variety of techniques, including software development alongside app development, as well as design and develop structures such as UI/UX, graphics, data management, and other analytical methods with a focus on business points.';
+  static const String aboutMeHobbiesTitle = 'Hobbies';
+  static const String aboutMeHobbiesDescription = 'Learning New Things\n'
+      'Watching Movies & TV Shows\n'
+      'Listening to Music & Dancing\n'
+      'Content Creation';
+  static const String aboutMeGoalTitle = 'Goal';
+  static const String aboutMeGoalDescription = 'To grow personally and professionally, while making a positive impact on the community.';
+  static const String aboutMeBanner = 'You can find my';
+  static const String aboutMeBannerTitle = 'public profiles';
+  static const String aboutMeBannerWeb = 'Visit profile.aman.dev to find all my public profiles.';
+
+  // Education
+  static const String educationTitle = 'Education';
+  static const String educationDescription = 'My educational background is not specific in any particular way and has come from various places. In this section, you can find out about my formal and other sources from 2003 to now. So, here are my school, undergraduate institution, and other learning platforms that I\'ve been involving for education.';
+  static const String educationOrg1Title = 'Nirmal VidhyaPeeth Barwaha, M.P';
+  static const String educationOrg1Course1Name = 'Higher secondary [HSC]';
+  static const String educationOrg1Course1Grade = 'Combined Maths, Physics, and Chemistry';
+  static const String educationOrg1Course1Year = 'Attempted: 2016 & 2017';
+  static const String educationOrg1Course3Name = 'Education Period';
+  static const String educationOrg1Course3Year = 'Secondary: 2019 - 2020';
+  static const String educationOrg1Course3Year2 = 'Higher Secondary: 2022 - 2023';
+
+  static const String educationOrg2Title = 'Astral Institute Of Technology And Research Indore, M.P';
+  static const String educationOrg2Course1Name = 'B.Tech - Information Technology';
+  static const String educationOrg2Course1Grade = 'Running';
+  static const String educationOrg2Course1Year = '2023 - Ongoing';
+  static const String educationBanner = 'These Are Some Of My Other';
+  static const String educationBannerTitle = 'Online Learning Platforms';
+  static const String educationBannerWeb =
+      'Click here to find out more about my online learning platforms.';
+
+  // Experience
+  static const String experienceTitle = 'Experience';
+  static const String experienceDescription =
+      'Since I\'ve started to learn and apply my knowledge in different places. So I started working as a freelance solution developer, and with time, I fell in love with different technologies. I received industrial training in my second year, and now I try out my experience.';
+  static const String experienceOrg1Title = 'Aman Rathore';
+  static const String experienceOrg1Role = 'Mobile Application Developer';
+  static const String experienceOrg1Years = 'June 2022 - Present';
+  static const String experienceOrg1Vales = 'Mobile, Desktop, Web development, UI UX, Tech Support Assistant & Data Analysis.';
+  static const String experienceOrg2Title = 'Dollop Infotech';
+  static const String experienceOrg2Role = 'Trainee Software Developer (Flutter)';
+  static const String experienceOrg2Years = 'June 2022 - Present';
+  static const String experienceOrg2Vales = 'Android, Flutter, Dart, Firebase, Git, REST API, JSON Parsing & UI UX Designing.';
+  static const String experienceBanner = 'Find my working';
+  static const String experienceBannerTitle = 'toolkit';
+  static const String experienceBannerWeb =
+      'Visit toolkit.dileepabandara.dev for more details >>';
+
+
+
+  // Contact
+  static const String contactTitle = 'Contact';
+  static const String contactDescription =
+      'Hey! I appreciate you looking at my public profile. Please feel free to ask me anything or to discuss anything with me. I\'m happy to chat with and meet new people. Regardless of the fact that I don\'t use social media much, you can find me there. So find me and get in touch with me. Send me an email if you need to discuss anything serious with me. I\'ll get back to you in 12 to 24 hours.';
+  static const String contactBanner = 'Send me an email at';
+  static const String contactEmail = 'aman93406@gmail.com';
+
+}
